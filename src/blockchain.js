9ea9a3a201b7bdf060f7fb810c4278f32e5309e9
Farhaanlab <142780213+Farhaanlab@users.noreply.github.com>
2025-09-25 00:49:36 +0530

    Update blockchain.js

--- src/blockchain.js
+++ src/blockchain.js
@@ -34,6 +34,21 @@ class Block {
 }
 
 class Blockchain {
+	isChainValid() {
+  for (let i = 1; i < this.chain.length; i++) {
+    const currentBlock = this.chain[i];
+    const previousBlock = this.chain[i - 1];
+
+    if (currentBlock.hash !== currentBlock.calculateHash()) {
+      return false;
+    }
+    if (currentBlock.previousHash !== previousBlock.hash) {
+      return false;
+    }
+  }
+  return true;
+}
+
     constructor() {
         this.chain = [this.createGenesisBlock()];
         this.difficulty = 2;
